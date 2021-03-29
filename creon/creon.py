f4e88e0303606bb65ac4accce9165e87c471423e
luckyDaveKim <lucky_dave@naver.com>
2021-03-30 00:41:16 +0900

    feat: Creon 연동 개발

--- creon/creon.py
+++ creon/creon.py
@@ -1,58 +1,41 @@
 import win32com.client
 import ctypes
-from pywinauto import application
-import os, time
 
 
-class creon():
+class Creon():
+    def __init__(self):
+        self.__validateAdmin()
+        self.codeManager = self.CodeManager()
+        self.cybosManager = self.CybosManager()
+        self.tradeManager = self.TradeManager()
 
-    cpStatus = win32com.client.Dispatch('CpUtil.CpCybos')
-    cpTradeUtil = win32com.client.Dispatch('CpTrade.CpTdutil')
-    id = ""
-    pwd = ""
-    pwdcert = ""
-    def __init__(self, config):
-        pass
-
-    def run_creon_service(self):
-        os.system('taskkill /IM coStarter* /F /T')
-        os.system('taskkill /IM CpStart* /F /T')
-        os.system('taskkill /IM DibServer* /F /T')
-        os.system('wmic process where "name like \'%coStarter%\'" call terminate')
-        os.system('wmic process where "name like \'%CpStart%\'" call terminate')
-        os.system('wmic process where "name like \'%DibServer%\'" call terminate')
-
-        time.sleep(5)
-        app = application.Application()
-
-        #'E:\STARTER\coStarter.exe /prj:cp '
-        app.start("D:\WUBU\Creon\STARTER\coStarter.exe /prj:cp "
-                "/id:{} /pwd:{} /pwdcert:{} /autostart".format(self.id, self.pwd, self.pwdcert))
-
-        time.sleep(60)
-
-    def check_creon_run(self):
+    def __validateAdmin(self):
+        # 프로세스가 관리자 권한으로 실행 여부
         if not ctypes.windll.shell32.IsUserAnAdmin():
-            print('Not Admin')
-            return False
+            raise Exception('일반권한으로 실행됨. 관리자 권한으로 실행해 주세요')
 
-        if self.cpStatus.IsConnect == 0:
-            print('Not Connect')
-            return False
+    class CodeManager():
+        def __init__(self):
+            self.__controller = win32com.client.Dispatch('CpUtil.CpCodeMgr')
 
-        return True
+        def codeToName(self, code):
+            return self.__controller.CodeToName(code)
 
-    def check_creon_system(self):
-        if not ctypes.windll.shell32.IsUserAnAdmin():
-            print('Not Admin')
-            return False
+        def getGroupCodeList(self, groupCode):
+            return self.__controller.GetGroupCodeList(groupCode)
 
-        if self.cpStatus.IsConnect == 0:
-            print('Not Connect')
-            return False
+    class CybosManager():
+        def __init__(self):
+            self.__controller = win32com.client.Dispatch('CpUtil.CpCybos')
 
-        if self.cpTradeUtil.TradeInit(0) != 0:
-            print('Not Trade Initialize')
-            return False
+        def __validateConnect(self):
+            if self.__controller.IsConnect == 0:
+                raise Exception('Cybos가 정상적으로 연결되지 않았습니다.')
 
-        return True
+    class TradeManager():
+        def __init__(self):
+            self.__controller = win32com.client.Dispatch('CpTrade.CpTdUtil')
+
+        def __validateInit(self):
+            if self.__controller.TradeInit(0) != 0:
+                raise Exception('Trade가 정상적으로 초기화되지 않았습니다.')
